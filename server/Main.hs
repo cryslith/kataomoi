a8347cab5c9b7828cdccda6b32c7dfa19de6fdde
Lily Chung <lkdc@mit.edu>
2016-03-18 13:32:53 -0400

    check username validity on server

--- server/Main.hs
+++ server/Main.hs
@@ -32,10 +32,13 @@ addUser u c s = if Map.member u s then Nothing else Just $ Map.insert u c s
 removeUser :: User -> ServerState -> ServerState
 removeUser = Map.delete
 
+send :: J.JSON a => WS.Connection -> a -> IO ()
+send conn = WS.sendTextData conn . T.pack . J.encode
+
 sendUsers :: WS.Connection -> ServerState -> IO ()
-sendUsers conn s = WS.sendTextData conn $ T.pack $ J.encode $
-                   J.toJSObject [("type", J.showJSON "users"),
-                                 ("users", J.showJSONs (userList s))]
+sendUsers conn s = send conn $
+                     J.toJSObject [("type", J.showJSON "users"),
+                                   ("users", J.showJSONs (userList s))]
 
 sendAllUsers :: ServerState -> IO ()
 sendAllUsers s = Map.fold (\conn m -> (m >> (sendUsers conn s))) (return ()) s
@@ -49,12 +52,10 @@ forwardMessage s user t =
     Nothing -> return False
 
 sendError :: WS.Connection -> String -> String -> IO ()
-sendError conn s m = WS.sendTextData conn $ T.pack $ J.encode $
+sendError conn s m = send conn $
                      J.toJSObject [("type", J.showJSON "error"),
                                    ("error", J.showJSON s),
                                    ("message", J.showJSON m)]
-
--- clientMessages = ["initiate", "respond", "decrypt", "reveal", "confirm"]
 
 server :: MVar ServerState -> WS.ServerApp
 server mstate pending = do
@@ -67,22 +68,26 @@ server mstate pending = do
         y = do
           jsonData <- case J.decode jsonString of
                         J.Error e -> Nothing
-                        J.Ok jsonData -> Just $ Map.fromList $ J.fromJSObject jsonData
+                        J.Ok jsonData -> Just $ Map.fromList $
+                                                  J.fromJSObject jsonData
           t <- Map.lookup "type" jsonData
           if t == "join" then
             do
               name <- Map.lookup "name" jsonData
-              Just $ changeState mstate $ \s ->
-                   case addUser name conn s of
-                     Nothing -> do
-                                  sendError conn "user already exists" jsonString
-                                  return s
-                     Just s' -> do
-                                  n <- readIORef nameRef
-                                  writeIORef nameRef $ Just name
-                                  return $ case n of
-                                             Nothing -> s'
-                                             Just n' -> removeUser n' s'
+              Just $ if usernameOK name then
+                changeState mstate $ \s ->
+                  case addUser name conn s of
+                    Nothing -> do
+                                 sendError conn "user already exists"
+                                           jsonString
+                                 return s
+                    Just s' -> do
+                                 n <- readIORef nameRef
+                                 writeIORef nameRef $ Just name
+                                 return $ case n of
+                                            Nothing -> s'
+                                            Just n' -> removeUser n' s'
+               else sendError conn "invalid username" jsonString
           else if t == "client" then
             do
               sender <- Map.lookup "sender" jsonData
@@ -91,7 +96,8 @@ server mstate pending = do
               Just $ do
                 s <- readMVar mstate
                 x <- forwardMessage s recipient jsonString
-                if x then return () else sendError conn "no such recipient" jsonString
+                if x then return () else sendError conn "no such recipient"
+                                                   jsonString
           else if t == "quit" then Just $ disconnect conn nameRef
           else Nothing
     case y of
@@ -107,6 +113,13 @@ server mstate pending = do
         Nothing -> return ()
         Just name' -> changeState mstate $ \s -> return $ removeUser name' s
 
+usernameOK :: String -> Bool
+usernameOK username =
+  length username >= 1 &&
+  length username <= 8 &&
+  all (\c -> 'a' <= c && c <= 'z' ||
+             'A' <= c && c <= 'Z' ||
+             '0' <= c && c <= '9') username
 
 main :: IO ()
 main = do
